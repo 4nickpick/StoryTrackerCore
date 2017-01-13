a339fc6d9c02abb9a2886fdf1e3e15ab00831a93
Nicholas Pickering <2nickpick@gmail.com>
2017-01-13 01:36:56 -0500

    restyle internal forms

--- www/tabmin/modules/stories/form.php
+++ www/tabmin/modules/stories/form.php
@@ -1,150 +1,1 @@
-<?php TemplateSet::begin('scripts') ?>
-
-    <!-- tinyMCE -->
-    <script src="/js/tinymce/tinymce.min.js"></script>
-    <script src="/js/tinymce/tinymce-init.js"></script>
-    <link rel="stylesheet" href="/css/tinymce-custom.css" />
-
-    <script type="text/javascript" src="/tabmin/modules/stories/js/module.js"></script>
-	<script type="text/javascript">
-		AlertSet.handleRedirectWithAlert();
-	</script>
-
-<?php TemplateSet::end('scripts')?>
-
-<h1><?=$verb=='add'? 'New Story' : 'Edit ' . htmlentitiesUTF8($story->get_name()) ?></h1>
-
-<ul id="form_navigation">
-    <li id="menu_details" class="current"><a href="javascript:" onclick="formSectionShow('details');">Details</a></li>
-    <li id="menu_description"><a href="javascript:" onclick="formSectionShow('description');">Description</a></li>
-    <li id="menu_synopsis"><a href="javascript:" onclick="formSectionShow('synopsis');">Plot Synopsis</a></li>
-</ul>
-
-<form id="story_form" action="/stories/ajax/" method="post" class="tabmin_form" autocomplete="off" enctype="multipart/form-data">
-	<input type="hidden" name="verb" value="<?php echo $verb?>" />
-	<input type="hidden" name="stories_id" value="<?php echo (@$story ? $story->get_id() : '')?>" />
-	<input type="hidden" name="users_id" value="<?php echo ($currentUser->get_id())?>" />
-	<?php echo XSRF::html()?>
-	<table class="form_table">
-		<tbody id="form_section_details">
-			<tr>
-				<th align="left" colspan="2"><h2>Basic Information</h2></th>
-			</tr>
-            <tr>
-                <th align="left" width="150">Name:</th>
-                <td><input type="text" name="name" value="<?php P::out(@$story ? $story->get_name() : '')?>" /></td>
-            </tr>
-
-            <tr>
-                <th align="left" colspan="2">&nbsp;</th>
-            </tr>
-
-            <tr>
-                <th align="left">Series:</th>
-                <td>
-                    <?php
-                    if( $verb == 'add' )
-                    {
-                        ?>
-                        <select name="series_id" onchange="if(this.value=='new_series'){$('#new_series_name_row').show();} else {$('#new_series_name_row').hide().find('input').val('');;} ">
-                            <optgroup label="">
-                                <option value="no_series">Not Part of a Series</option>
-                                <option value="new_series">New Series</option>
-                            </optgroup>
-                            <?php
-                            $loader = new SeriesLoader();
-                            $series_all = $loader->loadByCurrentUser($currentUser->get_id());
-                            if( count($series_all) > 0 )
-                            {
-                                ?>
-                                <optgroup label="Existing Series">
-                                    <?php
-                                    foreach($series_all as $series)
-                                    {
-                                        $selected = '';
-                                        if( isset($story) && (@$series->get_id() == @$story->get_series()->get_id()) )
-                                        {
-                                            $selected = 'selected="selected"';
-                                        }
-                                        ?>
-                                        <option value="<?=$series->get_id()?>" <?= $selected ?>><?=htmlentitiesUTF8($series->get_name())?></option>
-                                    <?php
-                                    }
-                                    ?>
-                                </optgroup>
-                            <?php
-                            }
-                            ?>
-                        </select>
-                        <?php
-                    }
-                    else
-                    {
-                        if( $story->isPartOfSeries() )
-                        {
-                            echo htmlentitiesUTF8($story->get_series()->get_name());
-                        }
-                        else
-                        {
-                            echo 'Not Part of a Series';
-                        }
-                        ?>
-                        <input type="hidden" name="series_id" value="<?=$story->get_series()->get_id()?>" />
-                        <?php
-                    }
-                    ?>
-                </td>
-            </tr>
-            <tr id="new_series_name_row" style="display:none;">
-                <th align="left">New Series Name:</th>
-                <td>
-                    <input type="text" name="series_name" value="" />
-                </td>
-            </tr>
-
-        <!--<tr>
-            <th align="left">Main Image</th>
-            <td><input type="file" name="picture" /><?php P::out(@$story ? 'Leave blank to keep the same picture' : '')?></td>
-        </tr>-->
-        </tbody>
-        <tbody id="form_section_description">
-            <tr>
-                <th align="left" colspan="2"><h2>Description</h2></th>
-            </tr>
-            <tr>
-                <th align="left" colspan="2"><textarea class="content" id="description" name="description"><?php P::rint(@$story ? $story->get_description() : '')?></textarea></th>
-            </tr>
-        </tbody>
-
-        <tbody id="form_section_synopsis">
-            <tr>
-                <th align="left" colspan="2">
-                    <h2>Plot Synopsis</h2>
-                    <p>The Plot Synopsis is an important piece of content - make sure it's up to par!</p>
-                </th>
-            </tr>
-            <tr>
-                <th align="left" colspan="2"><textarea class="content" id="synopsis" name="synopsis"><?php P::rint(@$story ? $story->get_synopsis() : '')?></textarea></th>
-            </tr>
-        </tbody>
-
-		<tbody id="form_section_buttons">
-			<tr>
-                <td id="throbber_wrapper">
-                    <div id="throbber" style="display:none;"><img src="/images/throbber.gif"/></div>
-                </td>
-				<td><input class="save button" type="button" value="Save" onclick="formSave(this.form, '<?=$verb?>');"/></td>
-				<td><input class="view button" type="button" value="View" <?= $verb == 'add' ? 'disabled="disabled"' : '' ?> onclick="formView(this.form);" /></td>
-				<td>
-					<input class="cancel button" type="button" value="Cancel" onclick="
-						AlertSet.confirm('Are you sure? You will lose any unsaved data.', 
-						function(){window.location='/<?=$module?>/list/';});" />
-				</td>
-			</tr>
-		</tbody>
-	</table>
-</form>
-
-<script>
-    formSectionShow('<?=isset($_GET['synopsis']) ? 'synopsis' : 'details'?>');
-</script>
+<?php TemplateSet::begin('scripts') ?>    <!-- tinyMCE -->    <script src="/js/tinymce/tinymce.min.js"></script>    <script src="/js/tinymce/tinymce-init.js"></script>    <link rel="stylesheet" href="/css/tinymce-custom.css" />    <script type="text/javascript" src="/tabmin/modules/stories/js/module.js"></script><?php TemplateSet::end('scripts')?><h1><?=$verb=='add'? 'New Story' : 'Edit "' . htmlentitiesUTF8($story->get_name()) . '"'?></h1><ul id="form_navigation">    <li id="menu_details" class="current"><a href="javascript:" onclick="formSectionShow('details');">Details</a></li>    <li id="menu_description"><a href="javascript:" onclick="formSectionShow('description');">Description</a></li>    <li id="menu_synopsis"><a href="javascript:" onclick="formSectionShow('synopsis');">Plot Synopsis</a></li></ul><form id="story_form" action="/stories/ajax/" method="post" class="tabmin_form" autocomplete="off" enctype="multipart/form-data">	<input type="hidden" name="verb" value="<?php echo $verb?>" />	<input type="hidden" name="stories_id" value="<?php echo (@$story ? $story->get_id() : '')?>" />	<input type="hidden" name="users_id" value="<?php echo ($currentUser->get_id())?>" />	<?php echo XSRF::html()?>	<table class="form_table">		<tbody id="form_section_details">			<tr>				<th align="left" colspan="2"><h2>Basic Information</h2></th>			</tr>            <tr>                <th align="left" width="150">Name:</th>                <td><input type="text" name="name" value="<?php P::out(@$story ? $story->get_name() : '')?>" /></td>            </tr>            <tr>                <th align="left" colspan="2">&nbsp;</th>            </tr>            <tr>                <th align="left">Series:</th>                <td>                    <?php                    if( $verb == 'add' )                    {                        ?>                        <select name="series_id" onchange="if(this.value=='new_series'){$('#new_series_name_row').show();} else {$('#new_series_name_row').hide().find('input').val('');;} ">                            <optgroup label="">                                <option value="no_series">Not Part of a Series</option>                                <option value="new_series">New Series</option>                            </optgroup>                            <?php                            $loader = new SeriesLoader();                            $series_all = $loader->loadByCurrentUser($currentUser->get_id());                            if( count($series_all) > 0 )                            {                                ?>                                <optgroup label="Existing Series">                                    <?php                                    foreach($series_all as $series)                                    {                                        $selected = '';                                        if( isset($story) && (@$series->get_id() == @$story->get_series()->get_id()) )                                        {                                            $selected = 'selected="selected"';                                        }                                        ?>                                        <option value="<?=$series->get_id()?>" <?= $selected ?>><?=htmlentitiesUTF8($series->get_name())?></option>                                    <?php                                    }                                    ?>                                </optgroup>                            <?php                            }                            ?>                        </select>                        <?php                    }                    else                    {                        if( $story->isPartOfSeries() )                        {                            echo htmlentitiesUTF8($story->get_series()->get_name());                        }                        else                        {                            echo 'Not Part of a Series';                        }                        ?>                        <input type="hidden" name="series_id" value="<?=$story->get_series()->get_id()?>" />                        <?php                    }                    ?>                </td>            </tr>            <tr id="new_series_name_row" style="display:none;">                <th align="left">New Series Name:</th>                <td>                    <input type="text" name="series_name" value="" />                </td>            </tr>            <tr class="spacer">                <td colspan="2">&nbsp;</td>            </tr>        </tbody>        <tbody id="form_section_description">            <tr>                <th align="left" colspan="2"><h2>Description</h2></th>            </tr>            <tr>                <th align="left" colspan="2"><textarea class="content" id="description" name="description"><?php P::rint(@$story ? $story->get_description() : '')?></textarea></th>            </tr>            <tr class="spacer">                <td colspan="2">&nbsp;</td>            </tr>        </tbody>        <tbody id="form_section_synopsis">            <tr>                <th align="left" colspan="2">                    <h2>Plot Synopsis</h2>                    <p>The Plot Synopsis is an important piece of content - make sure it's up to par!</p>                </th>            </tr>            <tr>                <th align="left" colspan="2"><textarea class="content" id="synopsis" name="synopsis"><?php P::rint(@$story ? $story->get_synopsis() : '')?></textarea></th>            </tr>            <tr class="spacer">                <td colspan="2">&nbsp;</td>            </tr>        </tbody>    </table>    <table class="form_table form_table_buttons">		<tbody id="form_section_buttons">			<tr>                <td id="throbber_wrapper">                    <div id="throbber" style="display:none;"><img src="/images/throbber.gif"/></div>                </td>				<td><input class="save button" type="button" value="Save" onclick="formSave(this.form, '<?=$verb?>');"/></td>				<td><input class="view button" type="button" value="View" <?= $verb == 'add' ? 'disabled="disabled"' : '' ?> onclick="formView(this.form);" /></td>				<td>					<input class="cancel button" type="button" value="Cancel" onclick="						AlertSet.confirm('Are you sure? You will lose any unsaved data.',						function(){window.location='/<?=$module?>/list/';});" />				</td>			</tr>        </tbody>	</table></form><script>    formSectionShow('<?=isset($_GET['synopsis']) ? 'synopsis' : 'details'?>');</script>
--- www/tabmin/modules/stories/js/module.js
+++ www/tabmin/modules/stories/js/module.js
@@ -1,156 +1,1 @@
-	/* 
-		Form Button Handlers
-		these are used as onclick calls to each button on the module form
-		They should entirely replace the "onsubmit" functionality of forms
-		
-	*/
-	function formSave(form, verb)
-	{
-        throb('block');
-
-		tinymce.triggerSave();
-		var ajax_ok = handleAjaxForm(
-			form, 
-			function(resp) {				
-				if( verb == 'add' )
-				{
-					//if added a story, go to the story view screen
-					var url = '/stories/view/'+resp.id;
-					AlertSet.redirectWithAlert(url, resp);
-                    // don't clear throbber, redirecting...
-				}
-				else
-				{				
-					AlertSet.clear().addJSON(resp).showInCorner();
-                    throb('none');
-				}
-			}, 
-			function(resp) {
-				AlertSet.clear().addJSON(resp).showInCorner();
-                throb('none');
-			}
-		);
-			
-		return ajax_ok;
-	}
-	
-	function formView(form)
-	{
-        throb('block');
-
-		tinymce.triggerSave();
-		var ajax_ok = handleAjaxForm(
-			form, 
-			function(resp) {
-                window.location='/stories/view/'+resp.id;
-                // don't clear throbber, redirecting...
-            },
-			function(resp) {
-                AlertSet.clear().addJSON(resp).showInCorner();
-                throb('none');
-            }
-		);
-		
-		return ajax_ok;
-	}
-
-    /* formSectionShow() - show a different section of the form, "model, content, relationships..." */
-	
-	function formSectionShow(section_name)
-	{
-		$('tbody:not(#form_section_'+section_name+')').hide();
-		$('#form_section_buttons').show();
-		$('#form_section_'+section_name).show();
-
-        $('#form_navigation li.current').removeClass('current');
-        $('#menu_'+section_name).addClass('current');
-	}
-		
-	/*
-		*Init() functions run javascript to be run when a section is loaded or AJAX-refreshed
-		the page is loaded/refreshed via *updateContent()
-	*/
-	function listInit()
-	{
-		enableDragAndDrop("#sortable tbody", "td div.sort.action_button", '', listUpdatePriority, '', 'y', '');
-        enableListClick();
-	}	
-
-    /*
-        *StartUpdateCountdown() sets a timer to trigger *updateContent()
-        If *StartUpdateCountdown() is called a second time before the first timer is up,
-            that timer is cleared.
-    */
-	var timeoutHandle = ''; //defined as a global so it can be unset if this function is called again
-	function listStartUpdateCountdown()
-	{
-		if( timeoutHandle != '' )
-			window.clearTimeout(timeoutHandle);
-		
-		timeoutHandle = window.setTimeout(function(){listUpdateContent()},500);
-	}
-	
-	/* 
-		*UpdateContent() AJAX-refreshes the page's content
-	*/
-	function listUpdateContent()
-	{
-		sort_value = '';
-		if( document.getElementById('sort') )
-			sort_value = document.getElementById('sort').value;
-		
-		loadPieceByURL(
-			'/stories/list-content/', 's='+document.getElementById('s').value+
-				'&sort='+sort_value, 
-			'get', 
-			document.getElementById('list-content'),
-			function(){listInit();}, document.getElementById('throbber') );
-	}	
-
-    /*
-        Drag and Drop Callbacks
-        These are used by enableDragAndDrop, as callbacks, they should have two parameters:
-        selector - the selector to the draggable elements
-        moved_element - the place in the list the element was just moved to
-        dropped_id - element id which was just dropped
-    */
-	
-	function listUpdatePriority(selector, moved_element, dropped_id)
-	{
-		handleAjaxFromURL('/stories/ajax/', 'verb=list_update_priority&moved_element='+moved_element.index()+'&'+$( selector ).sortable("serialize"), 'post',
-			function(resp){listUpdateContent();}, function(resp){AlertSet.addJSON(resp).show();});
-			
-		listMouseUp();
-	}
-
-	function listMouseUp()
-	{
-		//reset the list coloring
-		$( "table.list_table tbody tr" ).each(function( index ) {
-			if( index % 2 == 0 )
-				$(this).attr('class', 'dark');
-			else
-				$(this).attr('class', 'light');
-		});
-	}
-
-    function enableListClick() {
-        $('table.list_table tbody tr').each(function(index, element) {
-            var data = $(element).attr("id");
-
-            if( data )
-                data = data.split("_");
-
-            $(element)
-                .find('td:not(td:last)')
-                .each(function(index, element){
-                    $(element).css('cursor', 'pointer');
-                    element.onclick = function(e) {
-                        //allow click on series_name span, as well as rest of td
-                        if(!$(e.target).hasClass('series_name') && e.target !== this)
-                            return;
-                        loadStory(data[1]);
-                    }
-                });
-        });
-    }
+	/* 		Form Button Handlers		these are used as onclick calls to each button on the module form		They should entirely replace the "onsubmit" functionality of forms			*/	function formSave(form, verb)	{        throb('block');		tinymce.triggerSave();		var ajax_ok = handleAjaxForm(			form, 			function(resp) {								if( verb == 'add' )				{					//if added a story, go to the story view screen					var url = '/stories/view/'+resp.id;					AlertSet.redirectWithAlert(url, resp);                    // don't clear throbber, redirecting...				}				else				{									AlertSet.clear().addJSON(resp).showInCorner();                    throb('none');				}			}, 			function(resp) {				AlertSet.clear().addJSON(resp).showInCorner();                throb('none');			}		);					return ajax_ok;	}		function formView(form)	{        throb('block');		tinymce.triggerSave();		var ajax_ok = handleAjaxForm(			form, 			function(resp) {                window.location='/stories/view/'+resp.id;                // don't clear throbber, redirecting...            },			function(resp) {                AlertSet.clear().addJSON(resp).showInCorner();                throb('none');            }		);				return ajax_ok;	}    /* formSectionShow() - show a different section of the form, "model, content, relationships..." */		function formSectionShow(section_name)	{		$('tbody:not(#form_section_'+section_name+')').hide();		$('#form_section_buttons').show();		$('#form_section_'+section_name).show();        $('#form_navigation li.current').removeClass('current');        $('#menu_'+section_name).addClass('current');	}			/*		*Init() functions run javascript to be run when a section is loaded or AJAX-refreshed		the page is loaded/refreshed via *updateContent()	*/	function listInit()	{		enableDragAndDrop("#sortable tbody", "td div.sort.action_button", '', listUpdatePriority, '', 'y', '');        enableListClick();	}	    /*        *StartUpdateCountdown() sets a timer to trigger *updateContent()        If *StartUpdateCountdown() is called a second time before the first timer is up,            that timer is cleared.    */	var timeoutHandle = ''; //defined as a global so it can be unset if this function is called again	function listStartUpdateCountdown()	{		if( timeoutHandle != '' )			window.clearTimeout(timeoutHandle);				timeoutHandle = window.setTimeout(function(){listUpdateContent()},500);	}		/* 		*UpdateContent() AJAX-refreshes the page's content	*/	function listUpdateContent()	{		sort_value = '';		if( document.getElementById('sort') )			sort_value = document.getElementById('sort').value;				loadPieceByURL(			'/stories/list-content/', 's='+document.getElementById('s').value+				'&sort='+sort_value, 			'get', 			document.getElementById('list-content'),			function(){listInit();}, document.getElementById('throbber') );	}	    /*        Drag and Drop Callbacks        These are used by enableDragAndDrop, as callbacks, they should have two parameters:        selector - the selector to the draggable elements        moved_element - the place in the list the element was just moved to        dropped_id - element id which was just dropped    */		function listUpdatePriority(selector, moved_element, dropped_id)	{		handleAjaxFromURL('/stories/ajax/', 'verb=list_update_priority&moved_element='+moved_element.index()+'&'+$( selector ).sortable("serialize"), 'post',			function(resp){listUpdateContent();}, function(resp){AlertSet.addJSON(resp).show();});					listMouseUp();	}	function listMouseUp()	{		//reset the list coloring		$( "table.list_table tbody tr" ).each(function( index ) {			if( index % 2 == 0 )				$(this).attr('class', 'dark');			else				$(this).attr('class', 'light');		});	}    function enableListClick() {        $('table.list_table tbody tr').each(function(index, element) {            var data = $(element).attr("id");            if( data )                data = data.split("_");            $(element)                .find('td:not(td:last)')                .each(function(index, element){                    $(element).css('cursor', 'pointer');                    element.onclick = function(e) {                        //allow click on series_name span, as well as rest of td                        if(!$(e.target).hasClass('series_name') && e.target !== this)                            return;                        loadStory(data[1]);                    }                });        });    }
